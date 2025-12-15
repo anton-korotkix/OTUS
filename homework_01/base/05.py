7eb86c7da5bc70fabde3a4459e466621469f8cee
anton-korotkix <anton-korotkix@ya.ru>
2025-12-15 15:14:25 +0300

    05.py

---
+++ homework_01/base/05.py
@@ -0,0 +1,21 @@
+"""
+Пользователь вводит через консоль 10 чисел. Программа должна определить максимальное число и вывести его в консоль.
+"""
+i = 0
+max = 0
+string = ""
+digstring = ""
+
+
+while i != 10:
+    dig = input("\nInput number: ")
+    string += dig + " "
+    if dig.isdigit():
+        if int(dig) > int(max):
+            numbstring += dig + " "
+            max = dig
+        i += 1
+    else:
+        continue
+else:
+    print(f"Full string: {string}\nNumber string: {numbstring}\nMax: {max}")

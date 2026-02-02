72cfa9113a00997cce27ede73f76d9224904d94c
anton-korotkix <anton-korotkix@ya.ru>
2026-02-02 18:44:58 +0300

    Доработка класса Car

--- homework_05/car.py
+++ homework_05/car.py
@@ -1,3 +1,20 @@
 """
 Создайте класс `Car`, наследник `Vehicle`
 """
+
+from homework_05.base import Vehicle
+from homework_05.engine import Engine
+
+
+class Car(Vehicle):
+    """
+    Автомобиль
+    """
+    
+    def __init__(self):
+        """Инициализатор"""
+        self.engine = None #атрибут engine
+    
+    def set_engine(self, engine: Engine):
+        """Установка engine"""
+        self.engine = engine

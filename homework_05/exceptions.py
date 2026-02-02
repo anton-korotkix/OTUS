9552d66041a7fc655d9db069234893446f45d8bc
anton-korotkix <anton-korotkix@ya.ru>
2026-02-02 17:33:51 +0300

    Описание исключений

--- homework_05/exceptions.py
+++ homework_05/exceptions.py
@@ -4,3 +4,15 @@
 - NotEnoughFuel
 - CargoOverload
 """
+
+class LowFuelError(Exception):
+    """Исключение по низкому уровню топлива"""
+    pass
+
+class NotEnoughFuel(Exception):
+    """Исключение по недостаточности топлива"""
+    pass
+
+class CargoOverload(Exception):
+    """Исключение по перегрузу"""
+    pass

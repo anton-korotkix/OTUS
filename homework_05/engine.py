e5be541a50d0d76a93187073be66a13f7aa939d0
anton-korotkix <anton-korotkix@ya.ru>
2026-02-02 17:46:30 +0300

    Описание датакласса Engine

--- homework_05/engine.py
+++ homework_05/engine.py
@@ -1,3 +1,12 @@
 """
 Создайте dataclass `Engine`
 """
+
+from dataclasses import dataclass
+
+
+@dataclass
+class Engine:
+    """Двигатель"""
+    volume: float = 1.6 #атрибут объема
+    pistons: int = 4 #атрибут количества цилиндров
